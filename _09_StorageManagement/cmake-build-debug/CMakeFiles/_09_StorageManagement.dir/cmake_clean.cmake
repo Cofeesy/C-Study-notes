260aa7bca86f19c6f5c9dbc2a3da37d82d57eec5
Cofeesy <2898939720@qq.com>
2022-07-24 21:35:32 +0800

    Learn-05

---
+++ _09_StorageManagement/cmake-build-debug/CMakeFiles/_09_StorageManagement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_09_StorageManagement.dir/main.c.obj"
+  "_09_StorageManagement.pdb"
+  "_09_StorageManagement.exe"
+  "_09_StorageManagement.exe.manifest"
+  "lib_09_StorageManagement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/_09_StorageManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
